d76ae8d0d1ea734bdfdbc91a1cfb92e61e77f74c
Daniel-Carapinha-Oliveira <carapinhadaniel@hotmail.com>
2025-09-06 20:22:41 +0100

    Renames project to "Exercise"

--- Exercise/urls.py
+++ Exercise/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for GymApp project.
+URL configuration for Exercise project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
