655c9c290446020c3354d4298c63b85385e3a683
Daniel-Carapinha-Oliveira <carapinhadaniel@hotmail.com>
2025-09-07 18:50:01 +0100

    Creates models: Muscle and BodyPart.

--- apps/workouts/models.py
+++ apps/workouts/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Muscle(models.Model):
+    name = models.CharField(
+        verbose_name='name',
+        max_length=100,
+        unique=True,
+    )
+
+    # ForeignKeys
+    body_part = models.ForeignKey(
+        'workouts.BodyPart',
+        on_delete=models.PROTECT,
+        related_name='muscles',
+        verbose_name='parte do corpo',
+    )
+
+    class Meta:
+        db_table = 'Muscle'
+        ordering = ['name']
+        verbose_name = 'muscle'
+        verbose_name_plural = 'muscles'
+
+    def __str__(self):
+        return f"{self.name} - {self.body_part.name}"
+
+
+class BodyPart(models.Model):
+    name = models.CharField(
+        verbose_name='name',
+        max_length=100,
+        unique=True,
+    )
+
+    class Meta:
+        db_table = 'BodyPart'
+        ordering = ['name']
+        verbose_name = 'body part'
+        verbose_name_plural = 'body parts'
+
+    def __str__(self):
+        return self.name
