f1caeeba1f3e7a15b9847683a5002939a6393732
Daniel-Carapinha-Oliveira <carapinhadaniel@hotmail.com>
2025-09-24 21:15:35 +0100

    adds more fields to filter in exercises page and undo button

--- apps/exercises/filters.py
+++ apps/exercises/filters.py
@@ -1,10 +1,13 @@
 import django_filters
 from .models import Exercise
 
+
 class ExerciseFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains')
-    muscle_part = django_filters.CharFilter(lookup_expr='icontains')
+    description = django_filters.CharFilter(lookup_expr='icontains')
+    workout_type = django_filters.ChoiceFilter(choices=Exercise.WorkoutType.choices)
+    muscle_part = django_filters.CharFilter(field_name='muscle_part__name', lookup_expr='icontains')
 
     class Meta:
         model = Exercise
-        fields = ['name', 'muscle_part']
+        fields = ['name', 'description', 'workout_type', 'muscle_part']
