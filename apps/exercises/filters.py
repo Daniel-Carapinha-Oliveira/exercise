6f41927cdcfc9098e28191c192fcca278bb3aad1
Daniel-Carapinha-Oliveira <carapinhadaniel@hotmail.com>
2025-09-30 19:31:28 +0100

    updates exercises page

--- apps/exercises/filters.py
+++ apps/exercises/filters.py
@@ -1,13 +1,24 @@
 import django_filters
-from .models import Exercise
+from .models import Exercise, BodyPart, Muscle
 
 
 class ExerciseFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains')
     description = django_filters.CharFilter(lookup_expr='icontains')
     workout_type = django_filters.ChoiceFilter(choices=Exercise.WorkoutType.choices)
-    muscle_part = django_filters.CharFilter(field_name='muscle_part__name', lookup_expr='icontains')
+    muscle = django_filters.ModelChoiceFilter(
+        queryset=Muscle.objects.all(),
+        field_name='muscle_part__muscle',
+        to_field_name='id',
+        label='Muscle'
+    )
+    body_part = django_filters.ModelChoiceFilter(
+        queryset=BodyPart.objects.all(),
+        field_name='muscle_part__muscle__body_part',
+        to_field_name='id',
+        label='Body Part'
+    )
 
     class Meta:
         model = Exercise
-        fields = ['name', 'description', 'workout_type', 'muscle_part']
+        fields = ['name', 'description', 'workout_type']
--- apps/exercises/views.py
+++ apps/exercises/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django_filters.views import FilterView
 from .filters import ExerciseFilter
-from apps.exercises.models import BodyPart, Exercise
+from apps.exercises.models import BodyPart, Exercise, Muscle
+
+def muscles(request):
+    all_muscles = Muscle.objects.all()
+    return render(
+        request,
+        "muscles.html",
+        {'current_page': 'exercises', 'muscles': all_muscles}
+    )
 
 def body_parts(request):
     all_body_parts = BodyPart.objects.all()
@@ -11,22 +19,18 @@ def body_parts(request):
         {'current_page': 'exercises', 'body_parts': all_body_parts}
     )
 
-def exercises(request, body_part_id):
-    exercises_queryset = Exercise.objects.filter(muscle_part__muscle__body_part_id=body_part_id)
-
-    return render(request, "exercises.html", {
-        'current_page': 'exercises',
-        'exercises_queryset': exercises_queryset
-    })
-
-class AllExercises(FilterView):
+class Exercises(FilterView):
     model = Exercise
     filterset_class = ExerciseFilter
-    template_name = 'exercise_list.html'
-    paginate_by = 1
+    template_name = 'exercises.html'
+    paginate_by = 2
 
-    def get_queryset(self):
-        # Optimize queries if you have related objects
-        return super().get_queryset()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Copy GET parameters and remove 'page' so it doesn't conflict with pagination
+        querydict = self.request.GET.copy()
+        querydict.pop('page', None)
+        context['querystring'] = querydict.urlencode()
+        return context
 
 # TODO: add api to allow to add exercise, create a user and give to recruiter.
