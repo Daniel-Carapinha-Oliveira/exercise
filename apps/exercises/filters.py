08d9f70d8b0eb4e8b0c6d665bea9b8b062d05751
Daniel-Carapinha-Oliveira <carapinhadaniel@hotmail.com>
2025-10-18 20:20:47 +0100

    improvements to exercises.html

--- apps/exercises/filters.py
+++ apps/exercises/filters.py
@@ -63,7 +63,7 @@ class ExerciseFilter(django_filters.FilterSet):
             child_field='muscle',
             model=Muscle,
             fk_field='body_part_id',
-            empty_default="Select a body part"
+            empty_default="Select a body part first"
         )
 
         set_dependent_queryset(
@@ -71,5 +71,5 @@ class ExerciseFilter(django_filters.FilterSet):
             child_field='muscle_part',
             model=MusclePart,
             fk_field='muscle_id',
-            empty_default="Select a muscle"
+            empty_default="Select a muscle first"
         )
