a8bf86cb6dc8e00fe5d349a17574107312c01b6a
Daniel-Carapinha-Oliveira <carapinhadaniel@hotmail.com>
2025-10-18 18:44:42 +0100

    improvements

--- bin/boot_exercise.sh
+++ bin/boot_exercise.sh
@@ -11,4 +11,40 @@ while true; do
   sleep 5
 done
 
+while true; do
+  python manage.py loaddata body_part_fixture.json
+  if [[ "$?" == "0" ]]; then
+    break
+  fi
+  echo loaddata body_part_fixture command failed, retrying in 5 secs...
+  sleep 5
+done
+
+while true; do
+  python manage.py loaddata muscle_fixture.json
+  if [[ "$?" == "0" ]]; then
+    break
+  fi
+  echo loaddata muscle_fixture command failed, retrying in 5 secs...
+  sleep 5
+done
+
+while true; do
+  python manage.py loaddata muscle_part_fixture.json
+  if [[ "$?" == "0" ]]; then
+    break
+  fi
+  echo loaddata muscle_part_fixture command failed, retrying in 5 secs...
+  sleep 5
+done
+
+while true; do
+  python manage.py loaddata exercise_fixture.json
+  if [[ "$?" == "0" ]]; then
+    break
+  fi
+  echo loaddata exercise_fixture command failed, retrying in 5 secs...
+  sleep 5
+done
+
 uwsgi --http "0.0.0.0:8000" --module exercise.wsgi --master --processes 4 --threads 2
