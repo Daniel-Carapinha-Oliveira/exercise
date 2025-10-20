779162c97449864764357c43156abd3139fcab67
Daniel-Carapinha-Oliveira <carapinhadaniel@hotmail.com>
2025-10-20 23:07:55 +0100

    creates MetaDataMixin.

---
+++ apps/core/mixins.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class MetaDataMixin(models.Model):
+    created_at = models.DateTimeField(
+        verbose_name='created at',
+        auto_now_add=True
+    )
+    deleted_at = models.DateTimeField(
+        verbose_name='deleted at',
+        null=True,
+        blank=True
+    )
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        verbose_name='created by'
+    )
+    deleted_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='deleted by'
+    )
