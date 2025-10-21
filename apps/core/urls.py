06ce0c9969685aad56a89ef092f3a7333c0b4769
Daniel-Carapinha-Oliveira <carapinhadaniel@hotmail.com>
2025-10-21 18:59:26 +0100

    refactor imports in apps.core.urls.py and apps.core.views.py

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homepage
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+from .views import homepage
 
 urlpatterns = [
     path('', homepage, name='homepage'),
