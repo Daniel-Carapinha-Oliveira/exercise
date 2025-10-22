a46bd97f43b72b94629650936068961f8bd2c6ec
Daniel-Carapinha-Oliveira <carapinhadaniel@hotmail.com>
2025-10-22 19:40:30 +0100

    - adds tests to TestMetaDataMixin

--- apps/core/tests/test_mixins.py
+++ apps/core/tests/test_mixins.py
@@ -1,8 +1,12 @@
 import pytest
 
+from django.contrib.auth import get_user_model
 from django.db import connection, models
 
 from apps.core.mixins import MetaDataMixin
+from apps.core.helpers import get_deleted_user
+
+User = get_user_model()
 
 pytestmark = pytest.mark.django_db
 
@@ -48,6 +52,11 @@ class TestMetaDataMixin:
 
         assert obj.created_at is not None
 
+    def test_created_by_field_not_null(self, create_database_object):
+        obj = create_database_object
+
+        assert obj.created_at is not None
+
     def test_deleted_at_field_null(self, create_database_object):
         obj = create_database_object
 
@@ -57,3 +66,27 @@ class TestMetaDataMixin:
         obj = create_database_object
 
         assert not obj.deleted_by
+
+    def test_fk_created_by_on_delete_set_to_deleted_user(self, user_factory):
+        user = user_factory()
+        obj = self.NewModelForTesting.objects.create(created_by=user)
+        user.delete()
+        obj.refresh_from_db()
+        deleted_user = get_deleted_user()
+        assert obj.created_by == deleted_user
+
+    def test_fk_deleted_by_on_delete_set_to_deleted_user(self, user_factory):
+        user = user_factory()
+        obj = self.NewModelForTesting.objects.create(created_by=user, deleted_by=user)
+        user.delete()
+        obj.refresh_from_db()
+        deleted_user = get_deleted_user()
+        assert obj.deleted_by == deleted_user
+
+    def test_foreign_keys_point_to_user_model(self):
+        created_by_field = self.NewModelForTesting._meta.get_field('created_by')
+        deleted_by_field = self.NewModelForTesting._meta.get_field('deleted_by')
+
+        # Check that both foreign keys point to the same user model
+        assert created_by_field.remote_field.model == User, "'created_by' should point to User model"
+        assert deleted_by_field.remote_field.model == User, "'deleted_by' should point to User model"
