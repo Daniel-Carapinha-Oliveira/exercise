e7f86653f507f20cd2a9ea3be8fe897f44ed49da
Daniel-Carapinha-Oliveira <carapinhadaniel@hotmail.com>
2025-10-23 22:25:59 +0100

    - adds comments to view homepage and helper get_deleted_user for app core.

--- apps/core/helpers.py
+++ apps/core/helpers.py
@@ -3,6 +3,15 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 def get_deleted_user():
+    """
+    Ensures there is a user in the database representing a "deleted" account.
+    If a user with username 'deleted_user' already exists, it retrieves that user.
+    Otherwise, it creates a new inactive, non-staff, non-superuser user with
+    predefined name and email values.
+
+    Returns the User object corresponding to the "deleted_user", either existing
+    or newly created.
+    """
     user, created = User.objects.get_or_create(
         username = 'deleted_user',
         first_name = 'Deleted',
--- apps/core/views.py
+++ apps/core/views.py
@@ -4,4 +4,14 @@ from exercise.version import __version__
 
 
 def homepage(request):
+    """
+    Renders the homepage view of the website using the "homepage.html" template.
+
+    Receives the HTTP request object.
+
+    Returns an HttpResponse object containing the rendered homepage.
+    Passes a context dictionary containing:
+        - current_page: identifies the active page as 'homepage'
+        - version: the current application version number
+    """
     return render(request, "homepage.html", {'current_page': 'homepage', 'version': __version__})
