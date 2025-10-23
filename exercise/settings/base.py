2e80710700d45391087ca82a2ea8b83a301d6999
Daniel-Carapinha-Oliveira <carapinhadaniel@hotmail.com>
2025-10-23 21:28:34 +0100

    - removes whitenoise

--- exercise/settings/base.py
+++ exercise/settings/base.py
@@ -65,8 +65,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # external
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'exercise.urls'
--- exercise/settings/production.py
+++ exercise/settings/production.py
@@ -1,5 +1,1 @@
 from exercise.settings.base import *
-
-# WHITENOISE
-WHITENOISE_USE_FINDERS = True
-WHITENOISE_MANIFEST_STRICT = False
