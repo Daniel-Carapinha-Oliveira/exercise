2a32a13d38935af5edf0372fcf5a5abc8255e6b2
Daniel-Carapinha-Oliveira <carapinhadaniel@hotmail.com>
2025-10-23 12:05:27 +0100

    - Registers user_factory to use in app exercises

--- apps/exercises/tests/conftest.py
+++ apps/exercises/tests/conftest.py
@@ -1,5 +1,6 @@
 from pytest_factoryboy import register
 
+from apps.core.factories import UserFactory
 from apps.exercises.factories import (
     MuscleGroupFactory,
     MuscleFactory,
@@ -11,3 +12,4 @@ register(MuscleGroupFactory)
 register(MuscleFactory)
 register(MusclePartFactory)
 register(ExerciseFactory)
+register(UserFactory)
